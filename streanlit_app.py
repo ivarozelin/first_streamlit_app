0fe89b36a84adcaf16ee0f20ba01a974af81e21e
Iva Rzozelin <134283235+ivarozelin@users.noreply.github.com>
2024-01-24 14:29:00 +0200

    Update streanlit_app.py
    new header for smoothies

--- streanlit_app.py
+++ streanlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 
-streamlit.title("My Parents New Healthy Diner")
+streamlit.title("Breakfast Favourites")
 streamlit.header('🥣 Breakfast Menu')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
