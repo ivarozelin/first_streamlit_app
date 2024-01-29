390681ba910056a3af8c757d331978e92617b1db
Iva Rzozelin <134283235+ivarozelin@users.noreply.github.com>
2024-01-29 15:58:10 +0200

    Update streanlit_app.py

--- streanlit_app.py
+++ streanlit_app.py
@@ -24,14 +24,19 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section do display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+if not fruit_choice:
+          streanmlit.error('Please select a fruit to get information')
+else:
+#streamlit.write('The user entered ', fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+except URLError as e:
+   streamlit.error()
 
-#expand the data in a reading format
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
 
 #add second fruit selection
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
