a3e4c0373a486aa47f5e18cafae80677e7987767
Iva Rzozelin <134283235+ivarozelin@users.noreply.github.com>
2024-01-29 13:32:06 +0200

    Update streanlit_app.py

--- streanlit_app.py
+++ streanlit_app.py
@@ -42,6 +42,5 @@ streamlit.dataframe(my_data_rows)
 
 #add second fruit selection
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
 fruit_added = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
 
